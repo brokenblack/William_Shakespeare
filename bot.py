a0bb8715d866c385e43b6732841fe0b3c83321f6
brokenblack <kiki802510@gmail.com>
2019-09-24 17:56:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,13 @@ others = dict({u'儚い': '* https://emos.plurk.com/03860276d09cb8c3af57f4e1f67d
 		u'喵': '\nhttps://emos.plurk.com/2855722f8a0cac58d9503de3999e3a16_w46_h48.jpeg 喔呀，可愛的小貓咪有什麼事呢? 這麼大的雨，待在這裡可是會感冒的喔?\n=== 五分鐘後 ===\nhttps://emos.plurk.com/cbf27bddd90675c4baf25fc7ce9c9ee9_w48_h47.jpeg 薰，你在做什麼?\nhttps://emos.plurk.com/c998f6c405b82415369ee41a5cecb8bb_w48_h48.gif 呦，千聖，我打算給迷失的小貓咪找個家哦\nhttps://emos.plurk.com/2c98e01f6ab8b6483815c43b263e76fc_w47_h48.jpeg ......把傘給我\nhttps://emos.plurk.com/8737ccc164cff4da40a56e1048e6d55e_w44_h48.jpeg ?\nhttps://emos.plurk.com/fff851e041880f422788fbc81f1ee159_w45_h48.jpeg 把傘給我，我們一起撐，這樣你也比較輕鬆吧?\nhttps://emos.plurk.com/0e6035708fcb34472c66bc025dce3c10_w48_h48.gif',
 		u'嗚欸': '啊，花音，你又迷路了嗎?',
 		u'嗚誒': '啊，花音，你又迷路了嗎? 我們一起走吧',
-		u'https://emos.plurk.com/a4ed9585c8b95b624483d95750b1bd0b_w48_h48.jpeg': '這不是千聖嗎，真是命運一般的邂逅啊',
+		u'莎莎': '我的名字不叫莎莎喔，小貓咪',
+		u'機車': '可愛的小貓咪，心情不好呢，真是儚い啊',
+		u'吃屎': '怎麼了小貓咪? 今天很暴躁呢',
+		u'瑪芬': '瑪芬最棒了，濫藍也是，還有77',
+		u'遲到': 'https://emos.plurk.com/0e6035708fcb34472c66bc025dce3c10_w48_h48.gif 呦，美咲你也要遲到了嗎？我也是哦 (馬蹄聲: 咯噔咯噔\nhttps://emos.plurk.com/8d84d891658b99b6aa60e0bfd7b96811_w48_h48.png 我已經懶得吐槽了',
+		u'Leo': '啊，Leo好久不見了，你還是一樣儚い呢',
+u'leo': 'https://emos.plurk.com/c998f6c405b82415369ee41a5cecb8bb_w48_h48.gif 你這樣扯著我可是很困擾的，小貓咪\nhttps://emos.plurk.com/7fda7cf2081d578fa8f7e7616468cac4_w48_h48.jpeg Leo是隻狗喔，**小薰**',
 		'': 'https://emos.plurk.com/af5e30f9838bee2712b3b82440b592e1_w48_h44.jpg'
 		})
 emojis = [u'https://emos.plurk.com/7e6738afd5272b8e86fca5f9ce76f6c0_w48_h48.gif',
@@ -64,7 +70,7 @@ while True:
 									{'plurk_id': pid,
 									'content': formatted_output,
 									'qualifier': ':' })
-			
+			time.sleep(3)
 
 		elif msg.get('type') == 'new_response':
 			pid = msg.get('plurk_id')
@@ -81,6 +87,7 @@ while True:
 										'content': '@%s: 所以說，就是這麼一回事' %uuu + emoji,
 										'qualifier': ':'})
 						not_found = False
+						time.sleep(3)
 						break
 				if not_found:
 					for ha in others.keys():
@@ -91,6 +98,7 @@ while True:
 											{'plurk_id': pid,
 											'content': '@%s: {}'.format(others[ha])%uuu, 
 											'qualifier': ':'})
+							time.sleep(3)
 							break
 
 		'''	else:
